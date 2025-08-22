45564830163ee2ff9d24f0aca45e48de80c5079d
KoffieMeneer <v.vanos@e-quest.nl>
2025-08-22 10:19:59 +0200

    test 0.3

--- public/script.js
+++ public/script.js
@@ -144,6 +144,12 @@ eventClick: function(info) {
                 locatie: location
             });
 
+        
+        app.delete('/api/reservations', (req, res) => {
+            console.log('DELETE binnengekomen:', req.method, req.url, req.query, req.headers);
+        });
+
+
             const response = await fetch(`/api/reservations?${params.toString()}`, {
                 method: 'DELETE',
                 headers: {
